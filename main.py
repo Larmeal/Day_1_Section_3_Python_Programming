cd3ff705ad14c9e5767978122e09ae61712e7d74
Chutdanai Thongsom <95965281+Larmeal@users.noreply.github.com>
2022-03-06 21:34:02 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,26 @@
+import pandas
+from pandas import DataFrame
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+
+data = pandas.read_csv('cost_revenue_dirty.csv')
+data.describe()
+
+X = DataFrame(data, columns=['production_budget_usd'])
+y = DataFrame(data, columns=['worldwide_gross_usd'])
+
+regression = LinearRegression()
+regression.fit(X, y)
+regression.coef_
+regression.intercept_
+
+plt.figure(figsize=(10,6))
+plt.scatter(X, y, alpha=0.3)
+plt.title('Film Cost vs Global Revenue')
+plt.xlabel('Production Budget $')
+plt.ylabel('Worldwide Gross $')
+plt.ylim(0, 3000000000)
+plt.xlim(0, 450000000)
+plt.show()
+
+regression.score(X, y)
